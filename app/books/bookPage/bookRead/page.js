3d259896602c508fce9991ecb36daed4e2cff85e
Ekaterina Lapenko <ekaterinalapenko8960@gmail.com>
2024-03-20 14:15:46 +0300

    Style read page

--- app/books/bookPage/bookRead/page.js
+++ app/books/bookPage/bookRead/page.js
@@ -1,7 +1,6 @@
 import styles from "./bookRead.module.css";
 import { SAMPLE_DATA } from "@/app/data";
 
-<style></style>;
 export default function BookRead() {
   return (
     <>
@@ -19,6 +18,37 @@ export default function BookRead() {
           >
             record_voice_over
           </span>
+          <div>
+            <span
+              class="material-symbols-outlined"
+              style={{
+                fontSize: "36px",
+                fontWeight: "300",
+              }}
+            >
+              format_align_left
+            </span>
+            <span
+              class="material-symbols-outlined"
+              style={{
+                fontSize: "36px",
+                fontWeight: "300",
+                marginLeft: "1rem",
+                marginRight: "1rem",
+              }}
+            >
+              format_align_justify
+            </span>
+            <span
+              class="material-symbols-outlined"
+              style={{
+                fontSize: "36px",
+                fontWeight: "300",
+              }}
+            >
+              format_align_right
+            </span>
+          </div>
           {/* </span> */}
           {/* </button> */}
           <div>
@@ -46,8 +76,28 @@ export default function BookRead() {
             {/* </button> */}
           </div>
         </div>
-        BookRead
-        <div>{SAMPLE_DATA[0].text}</div>
+        <div className={styles.textHead}>{SAMPLE_DATA[0].head}</div>
+        <div className={styles.textBook}>{SAMPLE_DATA[0].text}</div>
+        <div className={styles.arrows}>
+          <span
+            class="material-symbols-outlined"
+            style={{
+              fontSize: "88px",
+              fontWeight: "200",
+            }}
+          >
+            chevron_left
+          </span>
+          <span
+            class="material-symbols-outlined"
+            style={{
+              fontSize: "88px",
+              fontWeight: "200",
+            }}
+          >
+            chevron_right
+          </span>
+        </div>
       </div>
     </>
   );
