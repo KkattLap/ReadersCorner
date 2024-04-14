137f1e96a69c128787758a77404cec55df1e48f2
Ekaterina Lapenko <ekaterinalapenko8960@gmail.com>
2024-04-14 20:45:57 +0300

    Add poems from DB

--- client/app/poems/page.js
+++ client/app/poems/page.js
@@ -1,20 +1,45 @@
 import PoemCard from "./poemCard";
 import styles from "./page.module.css";
 import { SAMPLE_POEMS } from "../data";
-const runCallback = (cb) => {
-  return cb();
-};
-export default function Poems() {
+// const runCallback = (cb) => {
+//   return cb();
+// };
+
+async function getData() {
+  const res = await fetch("http://localhost:8080/AuthorsPoems", {
+    cache: "no-store",
+  });
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+  const data = res.json();
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+
+  return data;
+}
+
+export default async function Poems() {
+  const result = await getData();
   return (
     <>
       <div className={styles.poems}>
-        {runCallback(() => {
+        {/* {runCallback(() => {
           const row = [];
           for (var i = 0; i < 9; i++) {
             row.push(<PoemCard />);
           }
           return row;
-        })}
+        })} */}
+        {result.map((item) => (
+          <PoemCard
+            key={item.poem_id}
+            author={item.full_name}
+            poem={item.poem_name}
+            cover={item.portrait}
+          ></PoemCard>
+        ))}
       </div>
     </>
   );
--- client/app/poems/poemCard.js
+++ client/app/poems/poemCard.js
@@ -1,19 +1,19 @@
 import Image from "next/image";
 import styles from "./poemCard.module.css";
-import { SAMPLE_POEMS } from "../data";
+// import { SAMPLE_POEMS } from "../data";
 import { lora } from "../fonts";
 import Link from "next/link";
-export default function PoemCard() {
+export default function PoemCard({ author, poem, cover }) {
   return (
     <>
       <Link href="/poems/poemRead" className={styles.linkCard}>
         <div className={`${styles.poemCard} ${lora.className}`}>
           <div className={styles.portrait}>
-            <Image src={SAMPLE_POEMS[0].cover} fill alt="portrait"></Image>
+            <Image src={cover} fill alt="portrait" objectFit="cover"></Image>
           </div>
           <div className={styles.infPoem}>
-            <div className={styles.authorName}>{SAMPLE_POEMS[0].author}</div>
-            <div>{SAMPLE_POEMS[0].name}</div>
+            <div className={styles.authorName}>{author}</div>
+            <div>{poem}</div>
           </div>
         </div>
       </Link>
