49c5584d677102cb7aa24097e8ce1c8f7d28c958
Ekaterina Lapenko <ekaterinalapenko8960@gmail.com>
2024-06-05 20:18:16 +0300

    Add dictionary context

--- client/app/authContext.js
+++ client/app/authContext.js
@@ -1,6 +1,7 @@
 "use client";
 import { createContext, useState, useEffect } from "react";
-
+import jwt_decode from "jwt-decode";
+import Cookies from "js-cookie";
 export const AuthContext = createContext();
 async function getData() {
   const res = await fetch("http://localhost:3000/user", {
@@ -13,8 +14,20 @@ async function getData() {
 
   return data;
 }
+async function getUserDictionary() {
+  const res = await fetch("http://localhost:3000/userDictionary", {
+    cache: "no-store",
+  });
+  const data = res.json();
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return data;
+}
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(false);
+  const [dictionary, setDictionary] = useState(false);
 
   const handleUser = () => {
     const fetchData = async () => {
@@ -27,11 +40,24 @@ export const AuthProvider = ({ children }) => {
     };
     fetchData();
   };
+  const handleUserDictionary = () => {
+    const fetchDictionary = async () => {
+      try {
+        const response = await getUserDictionary();
+        setDictionary(response);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchDictionary();
+  };
   const exitUser = () => {
     setUser(false);
   };
   return (
-    <AuthContext.Provider value={{ user, handleUser, exitUser }}>
+    <AuthContext.Provider
+      value={{ user, dictionary, handleUser, handleUserDictionary, exitUser }}
+    >
       {children}
     </AuthContext.Provider>
   );
