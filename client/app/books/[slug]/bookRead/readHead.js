7d20598d154d312916ea228eb201f8f057199dd3
Ekaterina Lapenko <ekaterinalapenko8960@gmail.com>
2024-06-07 18:18:21 +0300

    Delete comment

--- client/app/books/[slug]/bookRead/readHead.js
+++ client/app/books/[slug]/bookRead/readHead.js
@@ -4,18 +4,6 @@ export default function ReadHead({ changeFontSize }) {
   return (
     <>
       <div className={styles.headPage}>
-        {/* <button className={styles.listen}> */}
-        {/* <span class="material-icons" className={styles.listen}> */}
-        {/* <span
-          className="material-symbols-outlined"
-          style={{
-            fontSize: "48px",
-            fontWeight: "300",
-          }}
-          // className={styles.listen}
-        >
-          record_voice_over
-        </span> */}
         <div
           onClick={changeFontSize}
           style={{ cursor: "pointer", userSelect: "none" }}
